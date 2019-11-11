876937d912a4faa0f258c40bba5b38aa28833ef3
Michael Hlista <hlista91@gmail.com>
2019-11-10 20:06:35 -0600

    trying bootstrap cdn

--- app/javascript/packs/index.jsx
+++ app/javascript/packs/index.jsx
@@ -6,8 +6,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Route} from 'react-router-dom'
 import App from '../components/App'
-import 'bootstrap/dist/js/bootstrap.min.js'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap'
 import 'popper.js/dist/popper.js'
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
