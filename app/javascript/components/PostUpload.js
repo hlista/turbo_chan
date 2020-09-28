0639bbf764aa9e0c6989bcb8bcea93832403cf7f
Michael Hlista <hlista91@gmail.com>
2020-09-27 19:12:54 -0500

    Working on a new Post Input Component That will looks like a Post with editable fields. Should look more user friendly and approachable

--- app/javascript/components/PostUpload.js
+++ app/javascript/components/PostUpload.js
@@ -12,9 +12,12 @@ class PostUpload extends Component {
     };
 
     this.handleTextChange = this.handleTextChange.bind(this);
-    this.handleFileUpload = this.handleFileUpload.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleSubjectChange = this.handleSubjectChange.bind(this);
+    this.handleImageClick = this.handleImageClick.bind(this);
+    this.hiddenFileInput = React.createRef();
+    this.handleFileChange = this.handleFileChange.bind(this);
+    this.reader = new FileReader();
   }
 
   handleSubmit(event){
@@ -60,53 +63,45 @@ class PostUpload extends Component {
         })
   }
 
-  handleFileUpload(event){
-    this.setState({file: event.target.files});
-  }
   handleTextChange(event){
     this.setState({content: event.target.value});
   }
   handleSubjectChange(event){
     this.setState({subject: event.target.value})
   }
+  handleImageClick(event){
+    this.hiddenFileInput.current.click();
+  }
+  handleFileChange(event){
+    this.setState({file: event.target.files});
+    this.reader.readAsDataURL(event.target.files[0])
+  }
   render () {
     return (
-        <div className='container'>
-      <form onSubmit={this.handleSubmit}>
-        {!this.props.op ? <div className='row justify-content-md-center'>
-            <div className='col-md-1'>
-                Subject
-            </div>
-            <div className='col-md-3'>
-                <input className="form-control" type="text" onChange={this.handleSubjectChange}/>
-            </div>
-        </div> : null}
-        <div className='row justify-content-md-center'>
-            <div className='col-md-1'>
-                Content
-            </div>
-            <div className='col-md-3'>
-                <textarea value={this.state.content} className="form-control" onChange={this.handleTextChange} />
-            </div>
+        <div className="post-container">
+              <form onSubmit={this.handleSubmit}>
+                    <div className="d-flex">
+                        <div className="card">
+                            <div className="card-header">
+                                Create a New Thread on /{this.props.board}/
+                            </div>
+                            <div className="card-body d-flex">
+                                {this.state.file ? <img onClick={this.handleImageClick} src={this.reader.result} style={{maxWidth: '100px', maxHeight: '100px', height: 'auto', width: 'auto'}}/> :<img onClick={this.handleImageClick} src="https://www.generationsforpeace.org/wp-content/uploads/2018/03/empty.jpg" style={{maxWidth: '100px', maxHeight: '100px', height: 'auto', width: 'auto'}}/>}
+                                <input type="file" ref={this.hiddenFileInput} onChange={this.handleFileChange} style={{display:'none'}} />
+                                <div ref={this.textRef}>
+                                <blockquote> {this.state.content ? this.state.content : "Your message goes here."}</blockquote>
+                                </div>
+
+                            </div>
+                            <div className="card-footer">
+                                <font color="red">{this.state.error}</font>
+                                <button type='submit' className='float-right'>Send</button>
+                            </div>
+                        </div>
+                    </div>
+                </form>
         </div>
-        <div className='row justify-content-md-center'>
-            <div className='col-md-1'>
-                Image
-            </div>
-            <div className='col-md-3'>
-                <input label='upload file' type='file' onChange={this.handleFileUpload} />
-            </div>
-        </div>
-        <div className='row justify-content-md-center'>
-            <div className='col-md-3'>
-                <font color="red">{this.state.error}</font>
-            </div>
-            <div className='col-md-1'>
-                <button type='submit' className='float-right'>Send</button>
-            </div>
-        </div>
-      </form>
-      </div>
+
     );
   }
 }
